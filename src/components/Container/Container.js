dcfd52e7a9866129a19f1a29bb0184c3e3caa944
Debashish D. Dev <hello@debashish.dev>
2022-03-26 13:44:28 +0600

    Added Side Area and Movies component

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,13 +1,15 @@
 import React from "react";
+import Movies from "../Movies/Movies";
+import Sidearea from "../Sidearea/Sidearea";
 
 const Container = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 md:px-10">
       <div className="md:col-span-3">
-        <h2>Hello Container</h2>
+        <Movies></Movies>
       </div>
       <div className="md:col-span-1">
-        <h2>Hello Sidebar</h2>
+        <Sidearea></Sidearea>
       </div>
     </div>
   );
---
+++ src/components/Sidearea/Sidearea.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidearea = () => {
+    return (
+        <div>
+            <h2>Hello Side Area</h2>
+        </div>
+    );
+};
+
+export default Sidearea;
