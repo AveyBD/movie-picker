9b3956034c27d0a43c4cc0753f5ddda81f2d60cf
Debashish D. Dev <hello@debashish.dev>
2022-03-26 14:44:58 +0600

    Sending Data to movie component

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,19 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Movies from "../Movies/Movies";
 import Sidearea from "../Sidearea/Sidearea";
 
 const Container = () => {
-    const [movies, setMovies] = useState([]);
-    useEffect(()=>{
-        fetch('database.json')
-        .then(res=>res.json())
-        .then(data=>setMovies(data))
-    },[])
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 md:px-10">
       <div className="md:col-span-3">
         <Movies></Movies>
-        <h2>Total Movie: {movies.length}</h2>
       </div>
       <div className="md:col-span-1">
         <Sidearea></Sidearea>
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Movie from '../Movie/Movie';
 
 const Movies = () => {
+    // loading data from database
+    const [movies, setMovies] = useState([]);
+    useEffect(()=>{
+        fetch('database.json')
+        .then(res=>res.json())
+        .then(data=>setMovies(data))
+    },[])
     return (
         <div>
-            <h2>List of Movies</h2>
+            {/* seding data to movie components */}
+         {
+            movies.map(movie=><Movie 
+                key={movie.id}
+                movie={movie}
+                ></Movie>)
+        }
         </div>
     );
 };
