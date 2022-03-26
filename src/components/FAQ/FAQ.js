616904872e65d4bf0207617827a5a0f466ca2a92
Debashish D. Dev <hello@debashish.dev>
2022-03-26 23:04:38 +0600

    Added FAQ

---
+++ src/components/FAQ/FAQ.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FAQ = () => {
+    return (
+        <div>
+            <div className='grid-cols-2'>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default FAQ;
