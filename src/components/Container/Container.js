e45b33fd991dee1735fd08361325cb3a7f1e439a
Debashish D. Dev <hello@debashish.dev>
2022-03-26 13:41:21 +0600

    Added Two Column layout

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Container = () => {
-    return (
-        <div>
-            <h2>Hello Container</h2>
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 md:px-10">
+      <div className="md:col-span-3">
+        <h2>Hello Container</h2>
+      </div>
+      <div className="md:col-span-1">
+        <h2>Hello Sidebar</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Container;
