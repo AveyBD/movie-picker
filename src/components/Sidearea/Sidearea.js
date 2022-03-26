2540be0bf961ec72bd48bab799dc9e8ad35f6ca1
Debashish D. Dev <hello@debashish.dev>
2022-03-26 16:32:37 +0600

    Fixed Sidearea

--- src/components/Sidearea/Sidearea.js
+++ src/components/Sidearea/Sidearea.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Sidearea = () => {
     return (
         <div>
-            <div className="h-screen sticky top-0 border-2 border-green-200 rounded">
+            <div className="h-screen sticky top-0 border-2 border-green-200 rounded shadow">
                 <h2 className='text-center font-bold text-2xl border-2 border-green-600 rounded'>Your List</h2>
             </div>
         </div>
