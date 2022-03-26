fb5e54d9f8b34b4e95dbaeac1ab43ab4ea84e8dc
Debashish D. Dev <hello@debashish.dev>
2022-03-26 15:33:38 +0600

    Added FontAwesome

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faList } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
 
-const Movie = ({movie}) => {
-    console.log(movie);
-    const {id, category, name, duration, price, date, ratings, director, img } = movie;
-    return (
-        <div className='rounded border-2 border-green-200 shadow-lg px-5'>
-            <img className="p-2 rounded border-2 shadow mt-2" src={img} alt={name} />
-            <h2>{name}</h2>
-            <h2>{category}</h2>
-            <h2>Director: {director}</h2>
-            <h2>{duration}</h2>
-            <h2>Release Date: {date}</h2>
-            <h3>IMDB Ratings: {ratings}</h3>
-            <h2>Price: ${price}</h2>
-
-        </div>
-    );
+const Movie = ({ movie }) => {
+  console.log(movie);
+  const { id, category, name, duration, price, date, ratings, director, img } =
+    movie;
+  return (
+    <div className="rounded border-2 border-green-200 shadow-lg px-5 h-200">
+      <div>
+        <img
+          className="p-2 rounded border-2 shadow mt-2"
+          src={img}
+          alt={name}
+        />
+        <h2 className="text-xl font-semibold text-center mt-2">{name}</h2>
+        <h2 className="font-thin text-right">
+          {category} / {duration} / {date}
+        </h2>
+        <h2>
+          <span className="font-bold">Director:</span> {director}
+        </h2>
+        <h3 className="flex">
+          <img
+            src="https://yts.mx/assets/images/website/logo-imdb.svg"
+            alt=""
+          />{" "}
+          : {ratings}
+        </h3>
+        <h2>
+          <span className="font-bold">Price</span>: ${price}
+        </h2>
+        <button className="bg-green-500 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded w-full">
+          <span className="pr-2">Add to List</span>
+          <FontAwesomeIcon icon={faList} />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Movie;
