dd112701d14974e607adcf5d1144469397de3e34
Debashish D. Dev <hello@debashish.dev>
2022-06-23 15:11:03 +0600

    Using Sweet Alart

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 import Movie from "../Movie/Movie";
 import MovieList from "../MovieList/MovieList";
 import Sidearea from "../Sidearea/Sidearea";
@@ -16,19 +17,27 @@ const Container = () => {
   const addToList = (movie) => {
     const newWish = [...wish, movie];
     if (newWish.length > 4) {
-      alert("You can not select more than 4 Movie")
-      return
-  }
+      alert("You can not select more than 4 Movie");
+      return;
+    }
     setWish(newWish);
     console.log(wish);
   };
 
-  // function for selected movie 
+  // function for selected movie
   const movieToWatch = () => {
-    const finalMovie = wish[Math.floor(Math.random()*wish.length)]
+    const finalMovie = wish[Math.floor(Math.random() * wish.length)];
     console.log(finalMovie);
-    alert(`Movie For you ${finalMovie.name} Directed by ${finalMovie.director}. Duration: ${finalMovie.duration}`);
-  }
+    Swal.fire({
+      title: finalMovie.name,
+      text: `Director: ${finalMovie.director} // Duration: ${finalMovie.duration}`,
+      imageUrl: finalMovie.img,
+      imageWidth: 150,
+      imageHeight: 250,
+      imageAlt: "Custom image",
+    });
+    // alert(`Movie For you ${finalMovie.name} Directed by ${finalMovie.director}. Duration: ${finalMovie.duration}`);
+  };
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 md:px-10 gap-5">
       <div className="md:col-span-3">
@@ -49,7 +58,10 @@ const Container = () => {
             ))}
           </div>
           <div className="px-2">
-            <button onClick={movieToWatch} className="px-5 py-2 bg-green-500 rounded mt-5 w-full text-white">
+            <button
+              onClick={movieToWatch}
+              className="px-5 py-2 bg-green-500 rounded mt-5 w-full text-white"
+            >
               Choose One
             </button>
             <button
